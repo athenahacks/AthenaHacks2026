e32c0ef397a6236780f0bd3990b2ca46caaa98b2
Michaela Chang <michaela.w.chang@gmail.com>
2024-02-05 13:42:26 -0800

    Bloomberg and chevron sponsors added.

--- src/lib/components/sections/Sponsors.svelte
+++ src/lib/components/sections/Sponsors.svelte
@@ -5,7 +5,24 @@
 <section id="sponsors">
 	<Header>Sponsors</Header>
 	<div class="sponsor-list">
-		<p><i>Coming soon...</i></p>
+		<!-- <p><i>Coming soon...</i></p> -->
+		<div class="section tier-1">
+			<img
+				class="macaron"
+				src="/images/macaron-gold.png"
+				alt="macaron decoration"
+			/>
+			<img
+				class="sponsor"
+				src="/images/sponsors/bloomberg-engineering.png"
+				alt="Bloomberg Engineering"
+			/>
+			<img
+				class="sponsor"
+				src="/images/sponsors/chevron.png"
+				alt="Chevron"
+			/>
+		</div>
 	</div>
 	<div class="thank-you-message">
 		<p>
@@ -21,6 +38,41 @@
 <style lang="scss">
 	.sponsor-list {
 		margin-bottom: 8em;
+		display: flex;
+		justify-content: center;
+
+		.sponsor {
+			max-height: 90px;
+
+			img {
+				object-fit: contain;
+			}
+		}
+
+		.section {
+			width: 50%;
+			display: flex;
+			gap: 1em;
+			justify-content: space-around;
+			padding: 3em;
+			border-radius: 1.25em 1.25em 0 1.25em;
+			border: $brown solid 3px;
+			filter: drop-shadow(0.4em 0.4em 0 $brown);
+			position: relative;
+
+			.macaron {
+				width: 100px;
+				height: 100px;
+				position: absolute;
+				top: -30px;
+				right: -30px;
+				filter: drop-shadow(-0.05em 0.15em 0 $brown);
+			}
+		}
+
+		.tier-1 {
+			background-color: $gold;
+		}
 	}
 	.thank-you-message {
 		text-align: center;
