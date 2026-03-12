952106ab5ef8ff9a1738531f09eeaf9eab1f9ecd
Sarah <sarah.yankun@gmail.com>
2026-03-12 12:39:23 -0700

    added mlh badge

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,9 @@
-<h2><slot /></h2>
+<h2>
+    <slot />
+    <div>
+        <a id="mlh-trust-badge" style="display:block;max-width:100px;min-width:60px;position:fixed;right:50px;top:0;width:10%;z-index:10000" href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=yellow" target="_blank"><img src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-yellow.svg" alt="Major League Hacking 2026 Hackathon Season" style="width:100%"></a>
+    </div>
+</h2>
 
 <style lang="scss">
     h2 {
